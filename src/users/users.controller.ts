b32efcd616b1ab71b076d1553fa13b3bfb87c10b
Allan Pimentel <allanpeter565@gmail.com>
2023-12-18 15:00:25 -0300

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Query, Post, Body, Put, Param, Delete, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+@Injectable()
+export class UsersController {
+constructor(private userService: UsersService){}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto)
+}
+
+  @Get()
+  findAll(@Query() query: any) {
+    return this.userService.findAll()
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} user`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} user`;
+  }
+}
